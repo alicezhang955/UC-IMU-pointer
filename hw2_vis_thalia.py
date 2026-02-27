cdfbc281e5ae97040a97d81d0513ed1a236d25de
Yifan Zhang <98489276+yifan200354@users.noreply.github.com>
2026-02-27 13:11:42 -0500

    thalia code

---
+++ hw2_vis_thalia.py
@@ -0,0 +1,97 @@
+import time
+from collections import deque
+import serial
+import matplotlib.pyplot as plt
+
+# config 
+port = "/dev/cu.usbmodem487F303F49801" # note to user: replace this with your port if it doesn't work
+baud_rate = 115200
+# maximum number of points that can be drawn 
+max_points = 35000
+velocity = 3
+refresh_rate = 1/30
+
+def main():
+    feather = serial.Serial(port, baud_rate, timeout=0.1)
+    time.sleep(1.0)         
+    feather.reset_input_buffer()
+    x_coordinates = deque(maxlen=max_points)
+    y_coordinates = deque(maxlen=max_points)
+    # current position in the canvas 
+    converted_x = 0.0
+    converted_y = 0.0
+    last_time = time.time()
+    next_plot = time.time()
+    is_recording = False
+    # plot setup 
+    plt.ion()
+    fig, ax = plt.subplots()
+    plt.show(block=False)
+    (line,) = ax.plot([], [], linewidth=2)
+    ax.set_aspect("equal", adjustable="datalim")
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax.set_title("Real-time Long Exposure Drawing via IMU Sensing")
+    current_color = "black"  
+    try:
+        while True:
+            feather_data = feather.readline()
+            if feather_data:
+                decoded_data = feather_data.decode("utf-8", errors="ignore").strip()
+                print("RAW LINE:", decoded_data)
+                if not decoded_data:
+                    continue
+                # detect if the user is recording the first time, or re-recording 
+                # start with an empty background 
+                if decoded_data == "Start":
+                    x_coordinates.clear()
+                    y_coordinates.clear()
+                    converted_x = 0.0 
+                    converted_y = 0.0
+                    is_recording = True
+                    last_time = time.time()
+                    continue
+                if decoded_data == "Stop":
+                    is_recording = False
+                if not is_recording:
+                    continue
+                data_parts = []
+                # proceed to parse IMU sensor data and selected color 
+                for part in decoded_data.split(","):
+                    data_parts.append(part.strip())
+                if len(data_parts) >= 2:
+                    # x and y coordinates 
+                    x = float(data_parts[0])
+                    y = float(data_parts[1])
+                    if len(data_parts) == 3:
+                        selected_color = data_parts[2]
+                        current_color = selected_color
+                        line.set_color(current_color)  
+                    now = time.time()
+                    time_since_last_update = now - last_time
+                    last_time = now
+                    converted_x += (x * velocity) * time_since_last_update
+                    converted_y += (y * velocity) * time_since_last_update
+                    x_coordinates.append(converted_x)
+                    y_coordinates.append(converted_y)
+            if time.time() >= next_plot:
+                next_plot += refresh_rate
+                if len(x_coordinates) > 2:
+                    line.set_data(x_coordinates, y_coordinates)
+                    ax.relim()
+                    ax.autoscale_view()
+                fig.canvas.draw()
+                fig.canvas.flush_events()
+    # save image via Ctrl + C
+    except KeyboardInterrupt:
+        filename = f"drawing_{int(time.time())}.png"
+        ax.axis("off")
+        fig.savefig(filename, dpi=300, bbox_inches="tight", pad_inches=0)
+        print("Drawing saved!", filename)
+    finally:
+        feather.close()
+        plt.ioff()
+        plt.show()
+
+if __name__ == "__main__":
+    main()
---
+++ integrated_code_thalia.py
@@ -0,0 +1,224 @@
+import time
+import math
+import board
+import displayio
+import digitalio
+import terminalio
+from adafruit_display_text import label
+import adafruit_icm20x
+import mahony
+
+# [backend] config 
+i2c = board.I2C()
+time.sleep(0.2)
+sensor = adafruit_icm20x.ICM20948(i2c)
+# magnetometer calibration
+mag_min = [-0.5764, 0.0097, -0.5362]
+mag_max = [0.4725, 0.9919, 0.4743]
+# Mahony AHRS filter  & update rate of the AHRS filter 
+ahrs_hz = 100
+ahrs = mahony.Mahony(ahrs_hz, 5, 100)
+time_between_ahrs_updates = int(1e9 / ahrs_hz)
+# data printing (streaming) rate; every 0.01 second
+time_between_prints = 0.01 
+# smoothing
+alpha = 0.5
+last_ahrs_update_time = time.monotonic_ns()
+last_print_time = time.monotonic()
+baseline_pitch = 0.0
+baseline_roll = 0.0
+current_x = 0.0
+current_y = 0.0
+
+# [backend] helpers
+def calibrate_baseline(sensor, calibration_length, calibration_countdown_label):
+    calibration_start_time = time.monotonic()
+    samples = 0
+    sum_roll = 0.0
+    sum_pitch = 0.0
+    time.sleep(0.1)
+    last_shown_time = None
+    while True:
+        current_time = time.monotonic()
+        time_passed = current_time - calibration_start_time
+        if time_passed >= calibration_length:
+            break
+        remaining_time = int(calibration_length - time_passed)
+        if remaining_time != last_shown_time:
+            calibration_countdown_label.text = f"Calibration stops in {remaining_time}s"
+            last_shown_time = remaining_time
+        apply_mahony_filter(sensor) 
+        sum_roll += ahrs.roll
+        sum_pitch += ahrs.pitch
+        samples += 1
+        time.sleep(1.0 / ahrs_hz)
+    baseline_roll = sum_roll / samples
+    baseline_pitch = sum_pitch / samples
+    return baseline_roll, baseline_pitch
+
+# normalize magnetometer values
+def map_range(x, in_min, in_max, out_min, out_max):
+    mapped = (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+    if out_min <= out_max:
+        if mapped < out_min:
+            return out_min
+        if mapped > out_max:
+            return out_max
+        return mapped
+    else:
+        if mapped < out_max:
+            return out_max
+        if mapped > out_min:
+            return out_min
+        return mapped
+
+# read data from sensors and feed into Mahony filter 
+def apply_mahony_filter(sensor):
+    mag_x, mag_y, mag_z = sensor.magnetic
+    gyro_x, gyro_y, gyro_z = sensor.gyro
+    acc_x, acc_y, acc_z = sensor.acceleration
+    # normalize values 
+    mag_x = map_range(mag_x, mag_min[0], mag_max[0], -1.0, 1.0)
+    mag_y = map_range(mag_y, mag_min[1], mag_max[1], -1.0, 1.0)
+    mag_z = map_range(mag_z, mag_min[2], mag_max[2], -1.0, 1.0)
+    # mounting adjustments 
+    gyro_z = -gyro_z
+    mag_y = -mag_y
+    # apply Mahony filter
+    ahrs.update(gyro_x, gyro_y, gyro_z, acc_x, acc_y, acc_z, mag_x, mag_y, mag_z)
+
+# compute new x and y using updated roll and pitch values 
+def compute_new_coordinates(baseline_roll, baseline_pitch, current_x, current_y):
+    # baseline correction
+    new_x = (ahrs.roll - baseline_roll) * 57.29578
+    new_y = (ahrs.pitch - baseline_pitch) * -57.29578  
+    # smoothing
+    new_x = new_x + alpha * (current_x - new_x)
+    new_y = new_y + alpha * (current_y - new_y)
+    return new_x, new_y
+
+# [frontend] config
+auto_recording_countdown = 10.0  
+stop_recording_countdown = 8.0  
+
+# [frontend] hardware setup (button, display)
+button = digitalio.DigitalInOut(board.D0)
+button.switch_to_input(pull=digitalio.Pull.UP)
+display = board.DISPLAY
+width = display.width
+height = display.height
+root = displayio.Group()
+display.root_group = root
+
+# [frontend] global variables
+mode = "landing"
+selected_color = None
+color_index = 0
+countdown_start_time = time.monotonic()
+recording_start_time = None
+recording_stop_time = None
+next_data_transmission = time.monotonic()
+# True = not pressed, False = pressed 
+recent_button_state = button.value 
+
+# [frontend] UI setup - choose drawing color, record drawing, and stop recording
+colors = ["black", "pink", "purple"]
+choose_color_ui = displayio.Group()
+instructions = [
+    label.Label(terminalio.FONT, text="Select a color for your drawing:", color=0xFFFFFF, x=5, y=15),
+    label.Label(terminalio.FONT, text="Press once = black",  color=0xFFFFFF, x=5, y=35),
+    label.Label(terminalio.FONT, text="Press twice = Pink",  color=0xFFFFFF, x=5, y=55),
+    label.Label(terminalio.FONT, text="Press again = Purple",color=0xFFFFFF, x=5, y=75),
+]
+countdown_label = label.Label(terminalio.FONT, text="", color=0xFFFFFF, x=5, y=height - 20)
+for instruction in instructions:
+    choose_color_ui.append(instruction)
+choose_color_ui.append(countdown_label)
+root.append(choose_color_ui)
+recording_ui = displayio.Group()
+recording_label = label.Label(terminalio.FONT, text="", color=0xFFFFFF, x=5, y=height // 2)
+calibration_countdown_label = label.Label(terminalio.FONT, text="", color=0xFFFFFF, x=5, y=height - 20)
+recording_ui.append(recording_label)
+recording_ui.append(calibration_countdown_label)
+recording_ui.hidden = True
+root.append(recording_ui)
+stop_ui = displayio.Group()
+stop_label = label.Label(terminalio.FONT, text="", color=0xFFFFFF, x=5, y=height // 2)
+stop_ui.append(stop_label)
+stop_ui.hidden = True
+root.append(stop_ui)
+
+# [frontend] main loop 
+while True:
+    current_time_s = time.monotonic()
+    current_button_state = button.value
+    pressed_event = (recent_button_state is True) and (current_button_state is False)
+    # process button presses 
+    if pressed_event:
+        # select drawing color 
+        if mode == "landing":
+            selected_color = colors[color_index]
+            color_index = (color_index + 1) % len(colors)
+        # stop recording 
+        elif mode == "recording":
+            print("Stop")
+            mode = "stopping"
+            recording_stop_time = current_time_s
+            stop_ui.hidden = False
+            choose_color_ui.hidden = True
+    # landing - select color, countdown 
+    if mode == "landing":
+        choose_color_ui.hidden = False
+        stop_ui.hidden = True
+        time_passed = current_time_s - countdown_start_time
+        remaining_time = int(auto_recording_countdown - time_passed)
+        if selected_color is not None:
+            picked = selected_color
+        else:
+            picked = "none"
+        countdown_label.text = f"You picked the color {picked}.\nRecording starting in {remaining_time}s" 
+        if remaining_time <= 0:
+            if selected_color is None:
+                # set to default color if no color selected by user
+                selected_color = "black" 
+            mode = "calibrating"
+            choose_color_ui.hidden = True
+            recording_ui.hidden = False
+            # 5-second calibration 
+            recording_label.text = "Hold device still.\nCalibrating!"
+            baseline_roll, baseline_pitch = calibrate_baseline(sensor, 5.0, calibration_countdown_label)
+            calibration_countdown_label.text = ""
+            current_x = 0.0
+            current_y = 0.0
+            mode = "recording"
+            print("Start")
+            last_print_time = time.monotonic()
+            recording_start_time = current_time_s
+            next_data_transmission = current_time_s
+            recording_label.text = "Currently recording!\nPress D0 to stop"
+    if mode == "recording":
+        # ahrs update
+        current_time_ns = time.monotonic_ns()
+        if (current_time_ns - last_ahrs_update_time) >= time_between_ahrs_updates:
+            last_ahrs_update_time = current_time_ns
+            apply_mahony_filter(sensor)
+        # stream data for visualization on laptop
+        if (current_time_s - last_print_time) >= time_between_prints:
+            last_print_time = current_time_s
+            current_x, current_y = compute_new_coordinates(baseline_roll, baseline_pitch, current_x, current_y)
+            print(f"{current_x:.4f},{current_y:.4f},{selected_color}")
+    # stop recording
+    if mode == "stopping":
+        recording_ui.hidden = True
+        time_passed = current_time_s - recording_stop_time
+        remaining_time = int(stop_recording_countdown - time_passed)
+        stop_label.text = f"Recording stopped!\nReturning to home screen in {remaining_time}s"
+        if remaining_time == 0:
+            mode = "landing"
+            countdown_start_time = current_time_s
+            selected_color = None
+            color_index = 0
+            stop_ui.hidden = True
+            choose_color_ui.hidden = False
+    recent_button_state = current_button_state
+    time.sleep(0.001)
